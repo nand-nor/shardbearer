6abbf0bef2b12f3cc93fa912832629b68bbd812a
nand-nor <a13xandra.cliff0rd@gmail.com>
2022-08-08 15:29:34 -0400

    [#10] add windows dev support to build.rs, bump versions

--- shardbearer-proto/build.rs
+++ shardbearer-proto/build.rs
@@ -1,12 +1,20 @@
 extern crate protoc_grpcio;
+use std::env;
 
 fn main() {
-    let herald_output = "proto/herald/";
-    let radiant_output = "proto/radiant/";
-    let ctrl_output = "proto/bondsmith/";
-    let common_output = "proto/";
 
-    let proto_root = "proto/";
+   // let host = env::var("HOST").unwrap();
+    let target = env::var("TARGET").unwrap();
+
+    let (herald_output, radiant_output, ctrl_output, common_output, proto_root) = {
+        if target.contains("pc-windows-gnu")  {
+            ("proto\\herald\\","proto\\radiant\\","proto\\bondsmith\\","proto\\","proto\\")
+        } else {
+            ("proto/herald/","proto/radiant/","proto/bondsmith/","proto/","proto/")
+        }
+    };
+
+
     println!("cargo:rerun-if-changed={}", proto_root);
     println!("cargo:rerun-if-changed={}", herald_output);
     println!("cargo:rerun-if-changed={}", radiant_output);
--- shardbearer-proto/src/lib.rs
+++ shardbearer-proto/src/lib.rs
@@ -22,11 +22,3 @@ pub mod bondsmith {
 pub mod common {
     include!(concat!("../proto/mod.rs"));
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
