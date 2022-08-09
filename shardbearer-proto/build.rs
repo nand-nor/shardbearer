e661dbaef78ef8a9ccfd61055db10ae483adf4bd
nand-nor <a13xandra.cliff0rd@gmail.com>
2022-08-09 13:06:29 -0400

    [#10] dont build boringssl for now

--- shardbearer-proto/build.rs
+++ shardbearer-proto/build.rs
@@ -1,16 +1,47 @@
 extern crate protoc_grpcio;
 use std::env;
+//extern crate protoc_rust_grpc;
 
 fn main() {
 
-   // let host = env::var("HOST").unwrap();
+    // let host = env::var("HOST").unwrap();
     let target = env::var("TARGET").unwrap();
 
-    let (herald_output, radiant_output, ctrl_output, common_output, proto_root) = {
-        if target.contains("pc-windows-gnu")  {
-            ("proto\\herald\\","proto\\radiant\\","proto\\bondsmith\\","proto\\","proto\\")
+    let (
+        herald_input,
+        herald_output,
+        radiant_input,
+        radiant_output,
+        bondsmith_input,
+        bondsmith_output,
+        common_input,
+        common_output,
+        proto_root
+    ) = {
+        if target.contains("pc-windows-gnu") {
+            (
+                "proto\\herald\\herald.proto",
+                "proto\\herald\\",
+                "proto\\radiant\\radiant.proto",
+                "proto\\radiant\\",
+                "proto\\bondsmith\\bondsmith.proto",
+                "proto\\bondsmith\\",
+                "proto\\common.proto",
+                "proto\\",
+                "proto\\"
+            )
         } else {
-            ("proto/herald/","proto/radiant/","proto/bondsmith/","proto/","proto/")
+            (
+                "proto/herald/herald.proto",
+                "proto/herald/",
+                "proto/radiant/radiant.proto",
+                "proto/radiant/",
+                "proto/bondsmith/bondsmith.proto",
+                "proto/bondsmith/",
+                "proto/common.proto",
+                "proto/",
+                "proto/"
+            )
         }
     };
 
@@ -18,31 +49,75 @@ fn main() {
     println!("cargo:rerun-if-changed={}", proto_root);
     println!("cargo:rerun-if-changed={}", herald_output);
     println!("cargo:rerun-if-changed={}", radiant_output);
-    println!("cargo:rerun-if-changed={}", ctrl_output);
+    println!("cargo:rerun-if-changed={}", bondsmith_output);
+    println!("cargo:rerun-if-changed={}", common_output);
+/*
+    protoc_rust_grpc::run(protoc_rust_grpc::Args {
+        out_dir: herald_output,
+        includes: &[proto_root],
+        input: &[herald_input],
+        rust_protobuf: true, // also generate protobuf messages, not just services
+        ..Default::default()
+    }).expect("protoc-rust-grpc");
 
-    protoc_grpcio::compile_grpc_protos(&["common.proto"], &[proto_root], &common_output, None)
-        .expect("Failure to compile common proto");
+    protoc_rust_grpc::run(protoc_rust_grpc::Args {
+        out_dir: radiant_output,
+        includes: &[proto_root],
+        input: &[radiant_input],
+        rust_protobuf: true, // also generate protobuf messages, not just services
+        ..Default::default()
+    }).expect("protoc-rust-grpc");
 
-    protoc_grpcio::compile_grpc_protos(
-        &["herald/herald.proto"],
-        &[proto_root],
-        &herald_output,
-        None,
-    )
-    .expect("Failure to compile herald proto");
-    protoc_grpcio::compile_grpc_protos(
-        &["radiant/radiant.proto"],
-        &[proto_root],
-        &radiant_output,
-        None,
-    )
-    .expect("Failure to compile radiant proto");
+    protoc_rust_grpc::run(protoc_rust_grpc::Args {
+        out_dir: bondsmith_output,
+        includes: &[proto_root],
+        input: &[bondsmith_input],
+        rust_protobuf: true, // also generate protobuf messages, not just services
+        ..Default::default()
+    }).expect("protoc-rust-grpc");
 
-    protoc_grpcio::compile_grpc_protos(
-        &["bondsmith/bondsmith.proto"],
-        &[proto_root],
-        &ctrl_output,
-        None,
-    )
-    .expect("Failure to compile shard proto");
+    protoc_rust_grpc::run(protoc_rust_grpc::Args {
+        out_dir: bondsmith_output,
+        includes: &[proto_root],
+        input: &[bondsmith_input],
+        rust_protobuf: true, // also generate protobuf messages, not just services
+        ..Default::default()
+    }).expect("protoc-rust-grpc");
+
+    protoc_rust_grpc::run(protoc_rust_grpc::Args {
+        out_dir: common_output,
+        includes: &[proto_root],
+        input: &[common_input],
+        rust_protobuf: true, // also generate protobuf messages, not just services
+        ..Default::default()
+    }).expect("protoc-rust-grpc");
+*/
+
+        protoc_grpcio::compile_grpc_protos(&["common.proto"], &[proto_root], &common_output, None)
+            .expect("Failure to compile common proto");
+
+        protoc_grpcio::compile_grpc_protos(
+            &["herald/herald.proto"],
+            &[proto_root],
+            &herald_output,
+            None,
+        )
+        .expect("Failure to compile herald proto");
+        protoc_grpcio::compile_grpc_protos(
+            &["radiant/radiant.proto"],
+            &[proto_root],
+            &radiant_output,
+            None,
+        )
+        .expect("Failure to compile radiant proto");
+
+        protoc_grpcio::compile_grpc_protos(
+            &["bondsmith/bondsmith.proto"],
+            &[proto_root],
+            &bondsmith_output,
+            None,
+        )
+        .expect("Failure to compile shard proto");
+
+
 }
